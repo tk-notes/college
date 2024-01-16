45ba53be3ed82d142de7ddc570d2be6b5b700406
TK <leandrotk100@gmail.com>
2024-01-16 06:59:30 -0300

    *

---
+++ matrix/add.js
@@ -0,0 +1,28 @@
+const add = (m1, m2) => {
+  let matrix = [];
+
+  for (let i = 0; i < m1.length; i++) {
+    let row = [];
+
+    for (let j = 0; j < m1[i].length; j++) {
+      row.push(m1[i][j] + m2[i][j]);
+    }
+
+    matrix.push(row);
+  }
+
+  return matrix;
+};
+
+console.log(
+  add(
+    [
+      [3, 5],
+      [1, 2],
+    ],
+    [
+      [2, -1],
+      [6, 4],
+    ]
+  )
+);
