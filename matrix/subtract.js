538aed088155df63b617c92a84c97db8b60e4c67
TK <leandrotk100@gmail.com>
2024-01-16 07:01:45 -0300

    *

--- matrix/subtract.js
+++ matrix/subtract.js
@@ -1,4 +1,8 @@
 const subtract = (m1, m2) => {
+  if (m1.length !== m2.length || m1[0].length != m2[0].length) {
+    throw new Error("Matrices should have the same order");
+  }
+
   let matrix = [];
 
   for (let i = 0; i < m1.length; i++) {
@@ -22,6 +26,7 @@ console.log(
     ],
     [
       [5, 3, 7],
+      [-2, 3, 1],
       [-2, 3, 1],
     ]
   )
