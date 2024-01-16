7f3c9e6b49269c3449a85baf77e13ae63b7e400f
TK <leandrotk100@gmail.com>
2024-01-16 07:02:12 -0300

    *

--- matrix/add.js
+++ matrix/add.js
@@ -1,4 +1,8 @@
 const add = (m1, m2) => {
+  if (m1.length !== m2.length || m1[0].length != m2[0].length) {
+    throw new Error("Matrices should have the same order");
+  }
+
   let matrix = [];
 
   for (let i = 0; i < m1.length; i++) {
